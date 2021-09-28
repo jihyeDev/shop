1569c0a2b988f37963c68d89e2750162b2c519a8
samsung <samsung@LAPTOP-RVQADFAO>
2021-09-28 17:57:21 +0900

    전자책의 상세정보 페이지에서 주문하는 기능 추가 주문하기라는 버튼을 생성하여 클릭하면 주문 테이블에 주문 정보들이 insert되는 페이지와 메서드를 생성함

--- shop/src/dao/OrderDao.java
+++ shop/src/dao/OrderDao.java
@@ -256,4 +256,42 @@ public class OrderDao {
 		//list를 return
 		return list;
 	}
+	
+	// [회원] 전자책을 주문하는 메서드
+	// Oder 객체로 입력받아온 값을 DB에 insert 함
+	public boolean insertOrder(Order order) throws ClassNotFoundException, SQLException {
+		boolean result = false;
+		
+		// 매개변수 값을 디버깅
+		System.out.println(order.getEbookNo() + "<--- OrderDao.insertOrder parem : EbookNo");
+		System.out.println(order.getMemberNo() + "<--- OrderDao.insertOrder parem : MemberNo");
+		System.out.println(order.getOrderPrice() + "<--- OrderDao.insertOrder parem : OrderOrderPrice");
+		
+		// DB 실행
+		// dbUtil 객체 생성
+		DBUtil dbUtil = new DBUtil();
+		// dbUtil의 getConnection메서드를 사용하여 DB 연결
+		Connection conn = dbUtil.getConnection();
+		System.out.println(conn + "<--- conn");
+		String sql = "INSERT INTO orders(ebook_no, member_no, order_price, create_date, update_date) VALUES (?,?,?,NOW(),NOW())";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, order.getEbookNo());
+		stmt.setInt(2, order.getMemberNo());
+		stmt.setInt(3, order.getOrderPrice());
+		// 디버깅 코드 : 쿼리내용과 표현식의 파라미터값 확인가능
+		System.out.println(stmt + "<--- stmt");
+		
+		// INSERT 실행
+		int row = stmt.executeUpdate();
+		if(row == 1) {
+			result = true;
+		}
+		
+		// 종료
+		stmt.close();
+		conn.close();
+		
+		// 성공 : result = true, 실패 : false
+		return result;
+	}
 }
